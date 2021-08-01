d7bc4bf710fe49f78e96e2d0ab574e7526379955
yallu201 <dlruddlf07@gmail.com>
2021-08-01 16:56:19 +0900

    Convert interface 'Human' to class

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
 }
-const Person = {
-  name: "yallu",
-  age: 29,
-  gender: "male",
-};
+
+const yallu = new Human("yallu", 29);
 function sayHi(person: Human): string {
   return `Hello ${person.name}, you are ${person.age}`;
 }
 
-console.log(sayHi(Person));
+console.log(sayHi(yallu));
